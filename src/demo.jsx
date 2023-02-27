5a63a7f36d146ace5b0d70233f19fe57c6bc30d6
Craig Wilkinson <craig@crwi.uk>
2023-02-27 22:42:34 +0000

    Progress

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,178 +1,24 @@
-import { hic, insert, replace, append } from "./utils/vdom.js";
-import { style } from "./utils/style.js";
-import { atom, dep } from "./utils/atom.js";
+import { hic, apply, expand } from "./vdom.js";
+import { atom } from "./atom.js";
+import { style } from "./style.js";
 
 const mainEl = document.getElementById("main");
-const mousepadEl = document.getElementById("mousepad");
-const extraEl = document.getElementById("extra");
-const counterEditorEl = document.getElementById("editor");
-
-const Outlined = ({ children }) => (
-  <div style={style({border: '1px solid #445', padding: '10px'})}>
-    { children }
-  </div>
-);
-
-const counterAtom = atom(0);
-const Counter = ({ count, setCount }) => (
-  <Outlined>
-    <p>count is {count}</p>
-    <button click={() => setCount(count + 1)}>increment</button>
-  </Outlined>
-);
-
-
-counterAtom.addTrigger((count, setCount) => insert(mainEl, <Counter count={count} setCount={setCount} />));
-
-const ManyDots = ({ count, setCount }) => (
-  <p style={style({
-       color: count > 15 ? 'red'
-         : count > 10 ? 'orange'
-         : count > 5  ? 'yellow'
-         : 'white'
-     })}>
-    { new Array(count).fill('.') }
-  </p>
-);
-
-// Let's add a text box too
-const CounterEditor = ({ count, setCount }) => (
-  <input input={(e) => setCount(Number(e.target.value))} value={count} />
-);
-
-dep(
-  { count: counterAtom },
-  ({ count }) => {
-    const setCount = (newCount) => {
-      if (!isNaN(newCount)) {
-        count.set(newCount);
-      }
-    }
-
-    insert(counterEditorEl, <CounterEditor count={count.value} setCount={setCount} />);
-  });
-
-counterAtom.set(0);
-
-const otherCounterAtom = atom(0);
-otherCounterAtom.addTrigger((value) => insert(mainEl, <ManyDots count={value} />));
-
-insert(
-  mousepadEl,
-  <div>
-    <button click={() => otherCounterAtom.set(otherCounterAtom.value + 1)}>+</button>
-    <button click={() => otherCounterAtom.set(Math.max(0, otherCounterAtom.value - 1))}>-</button>
-  </div>
-);
-
-replace(document.getElementById('ellipse'), ({ children }) => <p>This used to be: { children }</p>);
-
-const myThings = [
-  {
-    name: "Craig",
-    age: 28
-  },
-  {
-    name: "Meg",
-    age: 30
-  },
-  {
-    name: "Geordi",
-    age: 0.4
-  }
-];
-
-for (var i = 0; i < 1000; i++) {
-  myThings.push({ name: `Player_${i}`, age: i });
-}
-
-const TableSearch = ({ search, setSearch, items }) => {
-  const results = items.filter(thing => thing.name.toLowerCase().includes(search.toLowerCase())
-                               || thing.age.toString().includes(search));
-
+const Outlined = ({ children }) => {
   return (
-    <div>
-      <input placeholder="Search table" input={(e) => setSearch(e.target.value)} value={search} />
-      <table style={style({ margin: '10px' })}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>An input</th>
-          </tr>
-        </thead>
-        <tbody>
-          { results.map(result => <tr>
-                                    <td>{ result.name }</td>
-                                    <td>{ result.age }</td>
-                                    <td><input /></td>
-                                  </tr>)}
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
-const searchTerm = atom('');
-searchTerm.addTrigger((search, setSearch) =>
-  insert(document.getElementById('searcher'),
-        <TableSearch search={search} setSearch={setSearch} items={myThings} />));
-
-searchTerm.set('Cra');
-
-const PackageJsonFetcher = ({ isFetching, result, onClickFetch }) => (
   <div>
-    { isFetching ? 'fetching...' : null }
-    { result ? `got ${result}` : null }
-    <button click={onClickFetch}>Click to get the author of this package</button>
+    <div style={style({border: '1px solid #445', padding: '10px'})}>
+      { children }
+    </div>
+
+    <div style={style({border: '1px solid #445', padding: '10px'})}>
+      { children }
+    </div>
   </div>
-);
+)};
 
-dep(
-  { isFetching: atom(false),
-    result: atom(null) },
-  ({ isFetching, result }) => {
-    const doFetch = async () => {
-      isFetching.set(true);
-      const fetchResult = await fetch('package.json');
-      const jsonResult = await fetchResult.json();
-      isFetching.set(false);
-      result.set(jsonResult.author);
-    }
+const el = <Outlined><Outlined><p>Hello world!</p></Outlined></Outlined>;
+const expanded = expand(el);
 
-    insert(document.getElementById('package-json-fetcher'), <PackageJsonFetcher
-                                                             isFetching={isFetching.value}
-                                                             result={result.value}
-                                                             onClickFetch={doFetch} />);
-  });
+const newEl = apply(expanded, mainEl);
 
-
-// Demo of a with wrapper
-const withCountRenders = (Child) => {
-  const renderCount = atom(0);
-
-  return () => {
-    renderCount.set(renderCount.value + 1);
-    return <Child renderCount={renderCount.value} />;
-  };
-};
-
-const ShowRenders = withCountRenders(({ renderCount }) => <p>This component has rendered { renderCount } times.</p>);
-
-const newEl = append(document.querySelector('main'), <ShowRenders />);
-append(document.querySelector('main'), <ShowRenders />);
-append(document.querySelector('main'), <ShowRenders />);
-
-replace(document.getElementById('fruit'),
-        ({ children }) => {
-          const vals = children
-                .childWithTag("tbody")
-                .childrenWithTag("tr")
-                .map(row => row.nthChild(1).nthChild(0))
-                .map(Number);
-                     
-          const total = vals.reduce((acc, curr) => acc + curr, 0);
-          children.childWithTag("tbody")
-            .push(<tr><td><b>Total</b></td><td>{ total }</td></tr>);
-          return children;
-        });
+apply(expanded, newEl);
--- src/vdom.ts
+++ src/vdom.ts
@@ -1,4 +1,4 @@
-type HicType = [any, any, ...any[]];
+class HicType extends Array {}
 
 interface Tagged {
   _hic?: HicType
@@ -7,20 +7,47 @@ interface Tagged {
 
 type TaggedElement = Tagged & Element & Node
 
-/**
- * Sort of hacky way to determine if some argument is a hic representation.
- */
-const isHic = (thing) =>
-      thing.length > 1
-      && typeof thing[1] === 'object'
-      && !Array.isArray(thing[1]);
+function isHic(a) {
+  return a instanceof HicType;
+}
 
 /**
    Conform to the jsx factory signature to produce hiccup.
 */
 export const hic = (name, options, ...children): HicType => {
-  return [name, options || {}, ...children];
+  const flatChildren = children.reduce((acc, child) => {
+    if (Array.isArray(child) && !isHic(child)) {
+      acc.push(...child);
+    } else {
+      acc.push(child);
+    }
+    return acc;
+  }, []);
+  return new HicType(name, options || {}, flatChildren);
 }
+
+/**
+   Given some hiccup, resolve any components to their resulting DOM only
+   hiccup. That is, only hiccup elements with lower case tag names should remain.
+   
+   This entails running the components with their attributes.
+*/
+export const expand = ([tag, attrs, children]: HicType): HicType => {
+  const expandedChildren = children
+    .map((child: HicType) => {
+      if (isHic(child)) {
+        return expand(child);
+      }
+
+      return child;
+    });
+
+  if (typeof tag === 'function') {
+    return expand(tag({ ...attrs, children: expandedChildren }));
+  }
+
+  return new HicType(tag, attrs, expandedChildren);
+};
 
 /**
  * Adds a dictionary representation of the HTMLElement
@@ -57,58 +84,35 @@ const updateAttrs = (el: TaggedElement, attrs: object) => {
 }
 
 const walk = (hic: HicType, node: Node | undefined, f: Function): any => {
-  const afterChildren = hic[2].map((child, idx) => {
+  const children = isHic(hic) ? hic[2] : [];
+  const afterChildren = children.map((child, idx) => {
     // TODO something smarter (like with react's key prop) to map nodes to elements in children.
     // Index is obviously not reliable
-    walk(child, node ? node.childNodes[idx] : undefined, f);
+    return walk(child, node ? node.childNodes[idx] : undefined, f);
   });
 
   return f(hic, afterChildren, node);
 }
 
 /**
- * Given some hiccup, create an HTML element.
- */
-const hiccupToElement = (hic: HicType, ns?: string): TaggedElement => {
-  return walk(hic, undefined, (currHic, childrenAfter) => {
-    if (!isHic(currHic)) {
-      return document.createTextNode(currHic);
-    }
-
-    // Otherwise this is a hiccup element.
-    // We don't need the original children.
-    const [tag, attrs] = currHic;
-
-    // Namespace must apply to all children too. If a namespace is set on an attribute, it should apply to all children.
-    const currentNS = attrs.xmlns || ns || (tag === 'svg' ? 'http://www.w3.org/2000/svg' : 'http://www.w3.org/1999/xhtml');
-    const newEl = document.createElementNS(currentNS, tag) as TaggedElement;
-    updateAttrs(newEl, attrs);
-
-    childrenAfter.forEach(child => {
-      newEl.append(child);
-    });
-
-    // The hic representation is stored on the HTML Element to make it easier to perform diffs.
-    // This is basically like a virtual dom but stored on the real dom for convenience.
-    newEl._hic = currHic;
-    return newEl;
-  })
-};
-
-
-/**
    Given some HTML element, update that element and its children with the hiccup.
    This preserves existing HTML elements without removing and creating new ones.
 */
 export const apply = (hic: HicType, el: TaggedElement) => {
-  return walk(hic, el, (currHic, childrenAfter, currEl) => {
-    const prevHic = currEl._hic ? currEl._hic : [undefined];
+  const parent = el.parentNode;
+  const result = walk(hic, el, (currHic, childrenAfter, currEl) => {
+    if (!isHic(currHic)) {
+      return document.createTextNode(currHic);
+    }
+
+    const prevHic = currEl?._hic ? currEl._hic : [undefined];
     const [prevTag] = prevHic;
     const [tag, attrs] = currHic;
 
     var result = currEl;
     if (prevTag !== tag || !result) {
-      result = hiccupToElement(hic);
+      const currentNS = attrs.xmlns || (tag === 'svg' ? 'http://www.w3.org/2000/svg' : 'http://www.w3.org/1999/xhtml');
+      result = document.createElementNS(currentNS, tag) as TaggedElement;
     }
 
     // TODO Make a little test (as in on a web page)
@@ -117,7 +121,7 @@ export const apply = (hic: HicType, el: TaggedElement) => {
     updateAttrs(result, attrs);
 
     childrenAfter.forEach((child, idx) => {
-      if (currEl.childNodes[idx] !== child) {
+      if (child && result.childNodes[idx] !== child) {
         // TODO Look into insertBefore(node) instead of append, so we can get the order right
         result.appendChild(child);
       }
@@ -128,27 +132,7 @@ export const apply = (hic: HicType, el: TaggedElement) => {
     result._hic = currHic;
     return result;
   });
+
+  parent?.replaceChild(result, el);
+  return result;
 }
-
-/**
-   Given some hiccup, resolve any components to their resulting DOM only
-   hiccup. That is, only hiccup elements with lower case tag names should remain.
-   
-   This entails running the components with their attributes.
-*/
-const expand = ([tag, attrs, ...children]: HicType): HicType => {
-  if (typeof tag === 'function') {
-    return expand(tag({ ...attrs, children }));
-  }
-
-  const renderedChildren = children
-        .map((child: HicType) => {
-          if (Array.isArray(child) && child.length) {
-            return expand(child);
-          }
-
-          return child;
-        });
-
-  return [tag, attrs, ...renderedChildren];
-};
