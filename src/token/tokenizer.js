dda2558814e9111fe6222b19e4ff83899c7058f0
Craig Wilkinson <craig@crwi.uk>
2023-03-05 12:11:09 +0000

    Add tokenizer code

--- src/token/tokenizer.js
+++ src/token/tokenizer.js
@@ -9,21 +9,78 @@ class ParseContext {
 }
 
 const isAlpha = (c) => {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    return (
+        (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= '0' && c <= '9'));
 }
 
-const parseAlphanumeric = (ctx) => {
-    const newToken = {
-        raw: "",
-    };
+const isWhitespace = (c) => {
+    return (c === '\n' || c === ' ' || c === '\t');
+}
 
-    while (isAlpha(ctx.raw[ctx.idx])) {
-        newToken.raw += ctx.raw[ctx.idx];
-        ctx.idx++;
+const parseWhile = (charCond) => (ctx) => {
+    const startIndex = ctx.idx;
+    let endIndex = startIndex;
+    while (charCond(ctx.raw[endIndex])) {
+        endIndex++;
     }
 
-    ctx.tokens.push(newToken);
-    return ctx;
+    if (endIndex > startIndex) {
+        return {
+            type: "UNSPECIFIED",
+            start: startIndex,
+            end: endIndex
+        };
+    }
 }
 
-console.log(parseAlphanumeric(new ParseContext("0123asdf hello")));
+const parseAlphanumeric = () => parseWhile(isAlpha);
+
+const parseWhitespace = () => parseWhile(isWhitespace);
+
+const parseToken = (token) => (ctx) => {
+    const startIndex = ctx.idx;
+    if (ctx.raw.slice(startIndex, startIndex + token.length) === token) {
+        return {
+            start: startIndex,
+            end: startIndex + token.length
+        };
+    }
+}
+
+const drop = (parser) => (ctx) => {
+    const result = parser(ctx);
+    if (!result) {
+        return;
+    }
+
+    ctx.idx += (result.end - result.start);
+}
+
+const keep = (parser, type) => (ctx) => {
+    const result = parser(ctx);
+    if (!result) {
+        return;
+    }
+
+    result.type = type;
+    ctx.tokens.push(result);
+    ctx.idx += (result.end - result.start);
+}
+
+const parseGo = () => (ctx) => {
+    drop(parseWhitespace())(ctx);
+    keep(parseToken("int"), "TOKEN_INT")(ctx);
+    drop(parseWhitespace())(ctx);
+    keep(parseToken("main"), "TOKEN_MAIN")(ctx);
+    drop(parseWhitespace())(ctx);
+}
+
+let ctx = new ParseContext("0123asdf hello");
+parseGo()(ctx);
+console.log(ctx)
+
+ctx = new ParseContext("     int main ");
+parseGo()(ctx);
+console.log(ctx)
